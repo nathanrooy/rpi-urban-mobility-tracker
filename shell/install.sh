799c99ef63716602fa2e583efd27428dc2159741
Jake <jake@hainesdata.science>
2021-03-13 17:19:42 -0500

    Updated install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -1,5 +1,7 @@
 sudo apt install wget -y
 wget https://raw.githubusercontent.com/hainesdata/rpi-urban-mobility-tracker/master/shell/init.sh
+wget https://raw.githubusercontent.com/hainesdata/rpi-urban-mobility-tracker/master/Dockerfile
 chmod +x init.sh
 UMT_DIR=${HOME}/umt_output && mkdir -p ${UMT_DIR}
+mv Dockerfile ${UMT_DIR}
 ./init.sh
